b6c8abce3f6521a110f60c8e5d4ad080e93c02e7
Ilya Bolkhovsky <ilyab@proekspert.ee>
2017-02-13 17:27:59 +0200

    add missing export

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ exports.Map = require('./components/Map.vue');
 exports.TileLayer = require("./components/TileLayer.vue");
 exports.Marker = require("./components/Marker.vue");
 exports.Polyline = require("./components/Polyline.vue");
+exports.Polygon = require("./components/Polygon.vue");
 exports.LayerGroup = require("./components/LayerGroup.vue");
 exports.IconDefault = require("./components/IconDefault.vue");
 exports.Tooltip = require("./components/Tooltip.vue");
