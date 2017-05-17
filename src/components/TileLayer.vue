35d22ea978e18c599ad930ac55c98bd63b572e00
Madd.is <github@madd.is>
2017-05-17 13:22:52 +0200

    Allow TileLayer url and attribution to change

--- src/components/TileLayer.vue
+++ src/components/TileLayer.vue
@@ -20,9 +20,22 @@ export default {
       if (this.token) this.params['token'] = this.token;
       this.$tileLayer = L.tileLayer(this.url, this.params);
     },
+    watch: {
+      url (val) {
+        this.$tileLayer.setUrl(val);
+      },
+      attribution (val, old) {
+        this.$attributionControl.removeAttribution(old);
+        this.$attributionControl.addAttribution(val);
+      },
+      token (val) {
+        this.params.token = val;
+      }
+    },
     methods: {
       deferredMountedTo(parent) {
         this.$tileLayer.addTo(parent);
+        this.$attributionControl = parent.attributionControl;
         var that = this.mapObject;
         for (var i = 0; i < this.$children.length; i++) {
           this.$children[i].deferredMountedTo(that);
