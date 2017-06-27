d7fbd01656fbc759ad4dabbee382bb63615dfa88
Mickael Bouchaud <mickael.bouchaud@gmail.com>
2017-06-27 13:51:30 +0200

    Fix issue when geojson is loaded asynchronously

--- examples/src/components/GeoJSON.vue
+++ examples/src/components/GeoJSON.vue
@@ -7,6 +7,7 @@
       <v-map style="height: 90%" :zoom="zoom" :center="center">
         <v-tilelayer :url="url" :attribution="attribution"></v-tilelayer>
         <v-geojson-layer :geojson="geojson" :options="options"></v-geojson-layer>
+        <v-geojson-layer :geojson="geojson" :options="options"></v-geojson-layer>
         <v-marker :lat-lng="marker"></v-marker>
       </v-map>
     </div>
@@ -15,7 +16,7 @@
 
 <script>
 import Vue2Leaflet from 'vue2-leaflet';
-import geojsonData from '../assets/geojson/france.json'
+import axios from 'axios';
 
 export default {
   name: 'example',
@@ -29,7 +30,7 @@ export default {
     return {
       zoom:6,
       center:[48, -1.219482],
-      geojson: geojsonData,
+      geojson: null,
       options: {
         style: function () {
           return {
@@ -45,6 +46,11 @@ export default {
       attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
       marker: L.latLng(47.413220, -1.219482),
     }
-  }
+  },
+  created () {
+    axios.get("https://rawgit.com/gregoiredavid/france-geojson/master/regions/pays-de-la-loire/communes-pays-de-la-loire.geojson").then(response => {
+      this.geojson = response.data;
+    });
+  },
 }
 </script>
