79c7bb69125069c87e361680c6863653f9844806
Mickael Bouchaud <mickael.bouchaud@gmail.com>
2017-10-02 15:13:05 +0200

    Add event for TileLayer

--- src/components/TileLayer.vue
+++ src/components/TileLayer.vue
@@ -4,6 +4,22 @@
 <script>
 
 import propsBinder from '../utils/propsBinder.js';
+import propsBinder from '../utils/eventsBinder.js';
+
+const events = [
+  'loading',
+  'tileunload',
+  'tileloadstart',
+  'tileerror',
+  'tileload',
+  'load',
+  'add',
+  'remove',
+  'popupopen',
+  'popupclose',
+  'tooltipopen',
+  'tooltipclose'
+];
 
 const props = {
   url: String,
@@ -29,6 +45,7 @@ export default {
       if (this.attribution) this.params['attribution'] = this.attribution;
       if (this.token) this.params['token'] = this.token;
       this.mapObject = L.tileLayer(this.url, this.params);
+      eventsBinder(this, this.mapObject, events);
       propsBinder(this, this.mapObject, props);
     },
     methods: {
