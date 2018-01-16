a18cb6763e904cb0c02aad4de858e71cdb796ce9
KoRiGaN <mickael.bouchaud@gmail.com>
2018-01-16 14:02:52 +0100

    Remove useless custom property

--- src/components/Marker.vue
+++ src/components/Marker.vue
@@ -51,7 +51,6 @@ const props = {
     custom: false,
   },
   options: {
-    custom: true,
     type: Object,
     default: () => ({}),
   },
@@ -92,9 +91,6 @@ export default {
       if (this.mapObject.dragging) {
         newVal ? this.mapObject.dragging.enable() : this.mapObject.dragging.disable();
       }
-    },
-    setOptions(newVal) {
-      Object.assign(this.options, newVal);
     },
     setVisible(newVal, oldVal) {
       if (newVal == oldVal) return;
