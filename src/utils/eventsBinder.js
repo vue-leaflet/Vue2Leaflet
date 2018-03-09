a14e8861bd51dddfe25256da3c35682aee22deec
Eric Xanderson <disarticulate@gmail.com>
2018-03-08 19:31:13 -0600

    Mouse movement, particularly, needs to be debounced
    I'd debounce as much as possible, maybe segregate. But vue crashes development mode with sub ms events.

--- src/utils/eventsBinder.js
+++ src/utils/eventsBinder.js
@@ -1,9 +1,10 @@
+let debounce = require('debounce');
 export default (vueElement, leaflet, events) => {
   for (var i = 0; i < events.length; i++) {
     const exposedName = 'l-' + events[i];
     const eventName = events[i];
-    leaflet.on(eventName, (ev) => {
+    leaflet.on(eventName, debounce((ev) => {
       vueElement.$emit(exposedName, ev);
-    });
+    }), 200);
   }
 }
