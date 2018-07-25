f51ff27ab6f333ea6d9e93843cab43c2865c1cfb
lordfuoco <lordfuoco@gmail.com>
2018-07-25 13:04:30 +0200

    feat: clone the slot of l-tooltip and unmount the slot from the dom after leaflet tooltip is instantiated

--- src/components/LTooltip.vue
+++ src/components/LTooltip.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <slot/>
+    <slot v-if="!ready"/>
   </div>
 </template>
 
@@ -23,13 +23,15 @@ export default {
   props: props,
   data () {
     return {
+      ready: false
     };
   },
   mounted () {
     this.mapObject = L.tooltip(this.options);
     L.DomEvent.on(this.mapObject, this.$listeners);
     propsBinder(this, this.mapObject, props);
-    this.mapObject.setContent(this.content || this.$el);
+    this.mapObject.setContent(this.content || this.$el.cloneNode(true));
+    this.ready = true;
     this.parentContainer = findRealParent(this.$parent);
     this.parentContainer.mapObject.bindTooltip(this.mapObject);
   },
