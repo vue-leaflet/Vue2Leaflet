c1df374ad5505dbd4685cdab88159b98248e17eb
lordfuoco <lordfuoco@gmail.com>
2018-09-26 09:24:24 +0200

    feat: Path, Circle, Polyline and Polygon base mixins

---
+++ src/mixins/Polygon.js
@@ -0,0 +1,13 @@
+import Polyline from './Polyline';
+
+export default {
+  mixins: [Polyline],
+  mounted () {
+    this.polygonOptions = {...this.polyLineOptions};
+  },
+  methods: {
+    getGeoJSONData () {
+      return this.mapObject.toGeoJSON();
+    }
+  }
+};
---
+++ src/mixins/Polyline.js
@@ -0,0 +1,58 @@
+import Path from './Path';
+
+export default {
+  mixins: [Path],
+  props: {
+    smoothFactor: {
+      type: Number,
+      custom: true,
+      default: 1.0
+    },
+    noClip: {
+      type: Boolean,
+      custom: true,
+      default: false
+    }
+  },
+  data () {
+    return {
+      ready: false
+    };
+  },
+  mounted () {
+    this.polyLineOptions = {
+      ...this.pathOptions,
+      smoothFactor: this.smoothFactor ? this.smoothFactor : undefined,
+      noClip: this.noClip
+    };
+  },
+  methods: {
+    setVisible (newVal, oldVal) {
+      if (newVal === oldVal) return;
+      if (newVal) {
+        this.parentContainer.addLayer(this);
+      } else {
+        this.parentContainer.removeLayer(this);
+      }
+    },
+    setLStyle (newVal, oldVal) {
+      if (newVal === oldVal) return;
+      this.mapObject.setStyle(newVal);
+    },
+    setSmoothFactor (newVal, oldVal) {
+      if (newVal === oldVal) return;
+      if (newVal) {
+        this.mapObject.setStyle({ smoothFactor: newVal });
+      }
+    },
+    setNoClip (newVal, oldVal) {
+      if (newVal === oldVal) return;
+      if (newVal) {
+        this.mapObject.setStyle({ noClip: newVal });
+      }
+    },
+    addLatLng (value) {
+      this.mapObject.addLatLng(value);
+    }
+  }
+};
