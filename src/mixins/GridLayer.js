d30d2fc603fccfa55c503d3f8fa095ca2b546ee2
lordfuoco <lordfuoco@gmail.com>
2018-09-26 10:20:15 +0200

    feat: GridLayer, TileLayer and TileLayerWMS base mixins

---
+++ src/mixins/GridLayer.js
@@ -0,0 +1,23 @@
+import Layer from './Layer';
+
+export default {
+  mixins: [Layer],
+  props: {
+    opacity: {
+      type: Number,
+      custom: false,
+      default: 1.0
+    },
+    zIndex: {
+      type: Number,
+      default: 1
+    }
+  },
+  mounted () {
+    this.gridLayerOptions = {
+      ...this.layerOptions,
+      opacity: this.opacity,
+      zIndex: this.zIndex
+    };
+  }
+};
---
+++ src/mixins/TileLayerWMS.js
@@ -0,0 +1,46 @@
+import TileLayer from './TileLayer';
+
+export default {
+  mixins: [TileLayer],
+  props: {
+    layers: {
+      type: String,
+      default: ''
+    },
+    styles: {
+      type: String,
+      default: ''
+    },
+    format: {
+      type: String,
+      default: 'image/jpeg'
+    },
+    transparent: {
+      type: Boolean,
+      custom: false
+    },
+    version: {
+      type: String,
+      default: '1.1.1'
+    },
+    crs: {
+      default: null
+    },
+    upperCase: {
+      type: Boolean,
+      default: false
+    }
+  },
+  mounted () {
+    this.tileLayerWMSOptions = {
+      ...this.tileLayerOptions,
+      layers: this.layers,
+      styles: this.styles,
+      format: this.format,
+      transparent: this.transparent,
+      version: this.version,
+      crs: this.crs,
+      upperCase: this.upperCase
+    };
+  }
+};
