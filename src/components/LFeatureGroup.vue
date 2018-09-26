287fdcdffc44fb34e0f39883f409b61111b4a68a
lordfuoco <lordfuoco@gmail.com>
2018-09-26 10:21:13 +0200

    feat: refactor FeatureGroup and LayerGroup with mixins methods

--- src/components/LFeatureGroup.vue
+++ src/components/LFeatureGroup.vue
@@ -7,18 +7,11 @@
 <script>
 import propsBinder from '../utils/propsBinder.js';
 import findRealParent from '../utils/findRealParent.js';
-
-const props = {
-  visible: {
-    type: Boolean,
-    custom: true,
-    default: true
-  }
-};
+import LayerGroup from '../mixins/LayerGroup.js';
 
 export default {
   name: 'LFeatureGroup',
-  props: props,
+  mixins: [LayerGroup],
   data () {
     return {
       ready: false
@@ -26,37 +19,12 @@ export default {
   },
   mounted () {
     this.mapObject = L.featureGroup();
-    propsBinder(this, this.mapObject, props);
+    propsBinder(this, this.mapObject, this.$options.props);
     L.DomEvent.on(this.mapObject, this.$listeners);
     this.ready = true;
     this.parentContainer = findRealParent(this.$parent, true);
     if (this.visible) {
       this.parentContainer.addLayer(this);
-    }
-  },
-  beforeDestroy () {
-    this.parentContainer.removeLayer(this);
-  },
-  methods: {
-    addLayer (layer, alreadyAdded) {
-      if (!alreadyAdded) {
-        this.mapObject.addLayer(layer.mapObject);
-      }
-      this.parentContainer.addLayer(layer, true);
-    },
-    removeLayer (layer, alreadyRemoved) {
-      if (!alreadyRemoved) {
-        this.mapObject.removeLayer(layer.mapObject);
-      }
-      this.parentContainer.removeLayer(layer, true);
-    },
-    setVisible (newVal, oldVal) {
-      if (newVal === oldVal) return;
-      if (newVal) {
-        this.parentContainer.addLayer(this);
-      } else {
-        this.parentContainer.removeLayer(this);
-      }
     }
   }
 };
--- src/components/LLayerGroup.vue
+++ src/components/LLayerGroup.vue
@@ -7,26 +7,11 @@
 <script>
 import propsBinder from '../utils/propsBinder.js';
 import findRealParent from '../utils/findRealParent.js';
-
-const props = {
-  visible: {
-    type: Boolean,
-    custom: true,
-    default: true
-  },
-  layerType: {
-    type: String,
-    custom: true
-  },
-  name: {
-    type: String,
-    custom: true
-  }
-};
+import LayerGroup from '../mixins/LayerGroup.js';
 
 export default {
   name: 'LLayerGroup',
-  props: props,
+  mixins: [LayerGroup],
   data () {
     return {
       ready: false
@@ -34,51 +19,12 @@ export default {
   },
   mounted () {
     this.mapObject = L.layerGroup();
-    propsBinder(this, this.mapObject, props);
+    propsBinder(this, this.mapObject, this.$options.props);
     L.DomEvent.on(this.mapObject, this.$listeners);
     this.ready = true;
     this.parentContainer = findRealParent(this.$parent);
     if (this.visible) {
       this.parentContainer.addLayer(this);
-    }
-  },
-  beforeDestroy () {
-    this.parentContainer.removeLayer(this);
-  },
-  methods: {
-    addLayer (layer, alreadyAdded) {
-      if (!alreadyAdded) {
-        this.mapObject.addLayer(layer.mapObject);
-      }
-      this.parentContainer.addLayer(layer, true);
-    },
-    removeLayer (layer, alreadyRemoved) {
-      if (!alreadyRemoved) {
-        this.mapObject.removeLayer(layer.mapObject);
-      }
-      this.parentContainer.removeLayer(layer, true);
-    },
-    setVisible (newVal, oldVal) {
-      if (newVal === oldVal) return;
-      if (newVal) {
-        this.parentContainer.addLayer(this);
-      } else {
-        this.parentContainer.removeLayer(this);
-      }
-    },
-    setName(newVal, oldVal) {
-      if (newVal === oldVal) return;
-      this.parentContainer.removeLayer(this);
-      if (this.visible) {
-        this.parentContainer.addLayer(this);
-      }
-    },
-    setLayerType(newVal, oldVal) {
-      if (newVal === oldVal) return;
-      this.parentContainer.removeLayer(this);
-      if (this.visible) {
-        this.parentContainer.addLayer(this);
-      }
     }
   }
 };
