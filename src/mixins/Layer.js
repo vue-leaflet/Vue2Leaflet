e54a084482cecb86a26ccf76c42c24f370d443b2
lordfuoco <lordfuoco@gmail.com>
2018-09-26 10:19:23 +0200

    feat: move more shared props and methods to Layer mixin

--- src/mixins/Layer.js
+++ src/mixins/Layer.js
@@ -7,6 +7,48 @@ export default {
     attribution: {
       type: String,
       default: null
+    },
+    name: {
+      type: String,
+      default: null
+    },
+    visible: {
+      type: Boolean,
+      custom: true,
+      default: true
+    }
+  },
+  mounted () {
+    this.layerOptions = {
+      attribution: this.attribution,
+      name: this.name,
+      pane: this.pane
+    };
+  },
+  beforeDestroy () {
+    this.parentContainer.removeLayer(this);
+  },
+  methods: {
+    setAttribution (val, old) {
+      let attributionControl = this.$parent.mapObject.attributionControl;
+      attributionControl.removeAttribution(old).addAttribution(val);
+    },
+    setName (newVal, oldVal) {
+      if (newVal === oldVal) return;
+      this.parentContainer.removeLayer(this);
+      if (this.visible) {
+        this.parentContainer.addLayer(this);
+      }
+    },
+    setVisible (newVal, oldVal) {
+      if (newVal === oldVal) return;
+      if (this.mapObject) {
+        if (newVal) {
+          this.parentContainer.addLayer(this);
+        } else {
+          this.parentContainer.removeLayer(this);
+        }
+      }
     }
   }
 };
