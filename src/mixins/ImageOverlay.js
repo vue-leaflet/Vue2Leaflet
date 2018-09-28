0004c41f46767e914259c9eb37590f2cd6a76ea8
lordfuoco <lordfuoco@gmail.com>
2018-09-28 09:01:37 +0200

    feat: image overlay mixin and migrate component to use the mixin

---
+++ src/mixins/ImageOverlay.js
@@ -0,0 +1,82 @@
+import Layer from './Layer.js';
+
+export default {
+  mixins: [Layer],
+  props: {
+    url: {
+      type: String,
+      custom: true
+    },
+    bounds: {
+      custom: true
+    },
+    opacity: {
+      type: Number,
+      custom: true,
+      default: 1.0
+    },
+    alt: {
+      type: String,
+      default: ''
+    },
+    interactive: {
+      type: Boolean,
+      default: false
+    },
+    crossOrigin: {
+      type: Boolean,
+      default: false
+    },
+    errorOverlayUrl: {
+      type: String,
+      custom: true,
+      default: ''
+    },
+    zIndex: {
+      type: Number,
+      custom: true,
+      default: 1
+    },
+    className: {
+      type: String,
+      default: ''
+    }
+  },
+  mounted () {
+    this.imageOverlayOptions = {
+      opacity: this.opacity,
+      alt: this.alt,
+      interactive: this.interactive,
+      crossOrigin: this.crossOrigin,
+      errorOverlayUrl: this.errorOverlayUrl,
+      zIndex: this.zIndex,
+      className: this.className
+    };
+  },
+  methods: {
+    setOpacity (opacity) {
+      return this.mapObject.setOpacity(opacity);
+    },
+    setUrl (url) {
+      return this.mapObject.setUrl(url);
+    },
+    setBounds (bounds) {
+      return this.mapObject.setBounds(bounds);
+    },
+    getBounds () {
+      return this.mapObject.getBounds();
+    },
+    getElement () {
+      return this.mapObject.getElement();
+    },
+    bringToFront () {
+      return this.mapObject.bringToFront();
+    },
+    bringToBack () {
+      return this.mapObject.bringToBack();
+    }
+  },
+  render () {
+    return null;
+  }
+};
