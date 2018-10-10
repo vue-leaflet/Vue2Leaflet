1a177c04eb2b5988efb2d635eb755ee0804687f3
Nicolò Maria Mezzopera <nico@pulilab.com>
2018-10-10 14:15:13 +0200

    fix: abandon renderer approach and resort to simple dom

--- src/components/LTooltip.vue
+++ src/components/LTooltip.vue
@@ -10,7 +10,7 @@ export default {
     this.mapObject = L.tooltip(this.popperOptions);
     L.DomEvent.on(this.mapObject, this.$listeners);
     propsBinder(this, this.mapObject, this.$options.props);
-    this.setContent();
+    this.mapObject.setContent(this.$el);
     this.parentContainer = findRealParent(this.$parent);
     this.parentContainer.mapObject.bindTooltip(this.mapObject);
   },
