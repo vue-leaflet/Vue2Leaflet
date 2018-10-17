fbb63f60efa617a8aafd02215699ee919f87e4bc
Nicolò Maria Mezzopera <nico@pulilab.com>
2018-10-17 15:18:15 +0200

    feat: options merger check if the props is default value
    give precedence to options set in the options object if the prop is left to it's default

--- src/utils/optionsUtils.js
+++ src/utils/optionsUtils.js
@@ -9,16 +9,20 @@ export const collectionCleaner = (options) => {
   return result;
 };
 
-export const optionsMerger = (props, options) => {
-  options = options && options.constructor === Object ? options : {};
+export const optionsMerger = (props, instance) => {
+  const options = instance.options && instance.options.constructor === Object ? instance.options : {};
   props = props && props.constructor === Object ? props : {};
   const result = {...collectionCleaner(options)};
   props = collectionCleaner(props);
+  const defaultProps = instance.$options.props;
   for (let key in props) {
-    if (result[key]) {
+    const def = defaultProps[key] ? defaultProps[key].default : Symbol('unique');
+    if (result[key] && def !== props[key]) {
       console.warn(`${key} props is overriding the value passed in the options props`);
+      result[key] = props[key];
+    } else if (!result[key]) {
+      result[key] = props[key];
     }
-    result[key] = props[key];
   };
   return result;
 };
