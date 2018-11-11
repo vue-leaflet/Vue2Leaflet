8ad84f566c8ceffe003c7c840d2f4c77fce01689
Nicolò Maria Mezzopera <nico@pulilab.com>
2018-11-11 14:40:38 +0100

    fix: example use own leaflet resources

--- examples/src/App.vue
+++ examples/src/App.vue
@@ -144,12 +144,12 @@ export default {
 
 <style>
 .leaflet-fake-icon-image-2x {
-  background-image: url(../../node_modules/leaflet/dist/images/marker-icon-2x.png);
+  background-image: url(../node_modules/leaflet/dist/images/marker-icon-2x.png);
 }
 .leaflet-fake-icon-shadow {
-  background-image: url(../../node_modules/leaflet/dist/images/marker-shadow.png);
+  background-image: url(../node_modules/leaflet/dist/images/marker-shadow.png);
 }
-@import "../../node_modules/leaflet/dist/leaflet.css";
+@import "../node_modules/leaflet/dist/leaflet.css";
 
 body {
   margin: 0px;
