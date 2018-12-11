23aa1fac85373a888be91dd1d446eb8fc05c4077
Nicolò Maria Mezzopera <nico@pulilab.com>
2018-12-11 10:03:46 +0100

    feat: if setter method does not exist do not bind anything, warning added

--- src/utils/propsBinder.js
+++ src/utils/propsBinder.js
@@ -7,6 +7,12 @@ export default (vueElement, leafletElement, props, options) => {
   for (var i = 0; i < keys.length; i++) {
     const key = keys[i];
     const setMethodName = 'set' + capitalizeFirstLetter(key);
+    if (!vueElement[setMethodName]) {
+      if (props[key].custom) {
+        console.warn('custom prop without matching setter: ', props[key]);
+      }
+      return;
+    }
     const deepValue = (props[key].type === Object) ||
       (props[key].type === Array) ||
       (Array.isArray(props[key].type));
@@ -23,6 +29,7 @@ export default (vueElement, leafletElement, props, options) => {
         deep: deepValue
       });
     } else {
+      // To Deprecate
       vueElement.$watch(key, (newVal, oldVal) => {
         leafletElement[setMethodName](newVal);
       }, {
