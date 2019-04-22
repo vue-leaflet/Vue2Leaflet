78d253e2b9c1222c84b71d512c37165ce869a35f
bezany <supermegacoder@gmail.com>
2019-04-22 15:30:59 +0300

    added getWrapperWithMap for wrap map component with root map

--- tests/unit/LMarker.spec.js
+++ tests/unit/LMarker.spec.js
@@ -5,32 +5,37 @@ import LMap from '@/components/LMap.vue';
 
 const localVue = createLocalVue();
 
-function getWrapper (propsData) {
-  const ParentMapVm = mount(LMap, {
+function getWrapperWithMap (component, propsData, mountOptions) {
+  const mapWrapper = mount(LMap, {
     localVue
-  }).vm;
+  });
 
   const wrapper = mount({
-    ...LMarker,
+    ...component,
     // trick from here https://github.com/vuejs/vue-test-utils/issues/560#issuecomment-461865315
     created () {
-      this.$parent = ParentMapVm;
+      this.$parent = mapWrapper.vm;
     }
   }, {
     localVue,
     propsData,
-    sync: false // avoid warning, see
+    sync: false, // avoid warning, see
     // Removing sync mode #1137 https://github.com/vuejs/vue-test-utils/issues/1137
+    ...mountOptions
   });
-  return wrapper;
+  return {
+    wrapper,
+    mapWrapper
+  };
 }
 
 describe('LMarker.vue', () => {
   test('LMarker.vue change prop latLng', async () => {
     const initLatlng = L.latLng([11, 22]);
-    const wrapper = getWrapper({
+    const wrapperAndMap = getWrapperWithMap(LMarker, {
       latLng: initLatlng
     });
+    const wrapper = wrapperAndMap.wrapper;
     expect(wrapper.exists()).toBe(true);
     expect(wrapper.vm.mapObject.getLatLng().equals(initLatlng)).toBe(true);
     const newLatLng = L.latLng([1, 1]);
@@ -38,5 +43,22 @@ describe('LMarker.vue', () => {
     await wrapper.vm.$nextTick();
     const curLatLng = wrapper.vm.mapObject.getLatLng();
     expect(curLatLng.equals(newLatLng)).toBe(true);
+  });
+
+  test('LMarker.vue default slot text', async () => {
+    const markerText = 'Hello from marker!';
+    const wrapperAndMap = getWrapperWithMap(LMarker, {
+      latLng: [0, 0]
+    }, {
+      slots: {
+        default: markerText
+      }
+    });
+    const wrapper = wrapperAndMap.wrapper;
+    const mapWrapper = wrapperAndMap.mapWrapper;
+    expect(mapWrapper.text()).toContain('Leaflet');
+    expect(wrapper.exists()).toBe(true);
+    await wrapper.vm.$nextTick();
+    expect(wrapper.text()).toEqual(markerText);
   });
 });
