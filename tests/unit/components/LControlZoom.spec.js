dda44883f365f65512ad82ca73f0c5ee9115b230
Michael Underwood <mike.underwood@gmail.com>
2019-04-22 20:08:03 -0600

    Reorganise tests to more closely match Leaflet API docs

--- tests/unit/components/LControlZoom.spec.js
+++ tests/unit/components/LControlZoom.spec.js
@@ -7,35 +7,41 @@ describe('component: LControlZoom.vue', () => {
     expect(wrapper.vm.mapObject).toBeDefined();
   });
 
-  test('LControlZoom.vue accepts and uses the control position option', () => {
+  // Leaflet Control options
+
+  test('LControlZoom.vue accepts and uses the Leaflet position option', () => {
     const { wrapper } = getWrapperWithMap(LControlZoom, { position: 'bottomleft' });
     expect(wrapper.vm.mapObject.getPosition()).toBe('bottomleft');
   });
 
-  test('LControlZoom.vue updates the mapObject when position prop is changed', async () => {
-    const { wrapper } = getWrapperWithMap(LControlZoom, { position: 'bottomleft' });
-    wrapper.setProps({ position: 'topright' });
-    await wrapper.vm.$nextTick();
-    expect(wrapper.vm.mapObject.getPosition()).toBe('topright');
-  });
+  // Leaflet Control.Zoom options
 
-  test('LControlZoom.vue includes zoomInText in rendered html', async () => {
+  test('LControlZoom.vue includes "zoomInText" in rendered html', async () => {
     const { mapWrapper } = getWrapperWithMap(LControlZoom, { zoomInText: 'BIGGER' });
     see(mapWrapper, 'BIGGER');
   });
 
-  test('LControlZoom.vue includes zoomOutText in rendered html', () => {
-    const { mapWrapper } = getWrapperWithMap(LControlZoom, { zoomOutText: 'smaller' });
-    see(mapWrapper, 'smaller');
-  });
-
-  test('LControlZoom.vue includes zoomInTitle in rendered html', () => {
+  test('LControlZoom.vue includes "zoomInTitle" in rendered html', () => {
     const { mapWrapper } = getWrapperWithMap(LControlZoom, { zoomInTitle: 'zoom in more' });
     see(mapWrapper, 'zoom in more');
   });
 
-  test('LControlZoom.vue includes zoomOutTitle in rendered html', () => {
+  test('LControlZoom.vue includes "zoomOutText" in rendered html', () => {
+    const { mapWrapper } = getWrapperWithMap(LControlZoom, { zoomOutText: 'smaller' });
+    see(mapWrapper, 'smaller');
+  });
+
+  test('LControlZoom.vue includes "zoomOutTitle" in rendered html', () => {
     const { mapWrapper } = getWrapperWithMap(LControlZoom, { zoomOutTitle: 'zoom out a bit' });
     see(mapWrapper, 'zoom out a bit');
+  });
+
+  // Leaflet Control set* methods
+
+  test('LControlZoom.vue updates the mapObject when the "position" prop is set', async () => {
+    const { wrapper } = getWrapperWithMap(LControlZoom, { position: 'bottomleft' });
+    wrapper.setProps({ position: 'topright' });
+    await wrapper.vm.$nextTick();
+    expect(wrapper.vm.mapObject.getPosition()).toBe('topright');
   });
 });
