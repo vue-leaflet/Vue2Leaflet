675efaf7167f2c843d4dd4b8b5ca42edba590bb3
Andi <andi23rosca@gmail.com>
2020-08-03 11:54:03 +0200

    Tile layer accepts both a string and a string array for the subdomains prop

--- src/mixins/TileLayer.js
+++ src/mixins/TileLayer.js
@@ -5,26 +5,34 @@ export default {
   props: {
     tms: {
       type: Boolean,
-      default: false
+      default: false,
     },
     subdomains: {
-      type: String,
-      default: 'abc'
+      type: [String, Array],
+      default: 'abc',
+      validator: prop => {
+        if (typeof prop === 'string') return true;
+        // Validates array that array only contains only strings
+        if (Array.isArray(prop)) {
+          return prop.every(subdomain => typeof subdomain === 'string');
+        }
+        return false;
+      },
     },
     detectRetina: {
       type: Boolean,
-      default: false
-    }
+      default: false,
+    },
   },
-  mounted () {
+  mounted() {
     this.tileLayerOptions = {
       ...this.gridLayerOptions,
       tms: this.tms,
       subdomains: this.subdomains,
-      detectRetina: this.detectRetina
+      detectRetina: this.detectRetina,
     };
   },
-  render () {
+  render() {
     return null;
-  }
+  },
 };
